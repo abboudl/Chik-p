6dc446794996201636d92fc2e978463f8d92e126
Louai Abboud <43652717+abboudl@users.noreply.github.com>
2021-06-15 00:26:47 -0400

    Update blizzardapi.py

--- blizzardapi.py
+++ blizzardapi.py
@@ -5,7 +5,9 @@ api_client = BlizzardApi("328a05ccfe304690aeff1d789ca2442a",
                          "c0t1do0AiCJMm54aXx821IJv2CFJhkjw")
 
 # API Endpoint
-categories_index = api_client.wow.game_data.get_achievement_categories_index("us", "en_US")
+categories_index = api_client.wow.game_data \
+        .get_achievement_categories_index("us", "en_US")
 
 # Wow Classic endpoint
-connected_realms_index = api_client.wow.game_data.get_connected_realms_index("us", "en_US", is_classic=True)
+connected_realms_index = api_client.wow.game_data \
+        .get_connected_realms_index("us", "en_US", is_classic=True)
