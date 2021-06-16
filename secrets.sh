4d2625dba64feab5468f762d5ea9518a8337d868
Louai Abboud <43652717+abboudl@users.noreply.github.com>
2021-06-15 21:24:39 -0400

    Create secrets.sh

---
+++ secrets.sh
@@ -0,0 +1,15 @@
+bash$ vault kv get secret/hello
+
+====== Metadata ======
+Key              Value
+---              -----
+created_time     2020-09-02T21:41:17.568155Z
+deletion_time    n/a
+destroyed        false
+version          2
+
+===== Data =====
+Key        Value
+---        -----
+MyUser    myPass
+
